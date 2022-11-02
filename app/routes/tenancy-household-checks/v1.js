06ca530d43787aaf6c4ada05500ad1540a598c6f
Westley Knight <westley.knight@gmail.com>
2022-11-02 17:01:53 +0000

    Addition of visit details and amends to tenant details

---
+++ app/routes/tenancy-household-checks/v1.js
@@ -0,0 +1,15 @@
+module.exports = function (router) {
+
+  var v = "v1";
+  var path = "/tenancy-household-checks/"+v+"/";
+
+  router.post(path+'tenants-details', function (req, res) {
+    /*if (req.session.data["eligibility-"+v+"-didyouapply"] == "Yes"){
+      res.redirect(path+'granted-refused-permission');
+    } else {
+      res.redirect(path+'shutter-did-you-apply');
+    }*/
+    res.redirect(path+'visit-details');
+  })
+
+}
