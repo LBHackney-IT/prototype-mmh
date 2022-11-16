8184c63fe664bd7e26a12c33084269ccfcd432bf
Westley Knight <westley.knight@gmail.com>
2022-11-16 16:28:41 +0000

    Add cautionary contact v1

--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
-require('./routes/tenancy-household-checks/v1.js')(router);
+
+// Tenancy & Household Checks (Tenancy Audit)
+  require('./routes/tenancy-household-checks/v1.js')(router);
+
+// Cautionary Contacts
+  require('./routes/cautionary-contacts/v1.js')(router);
 
 module.exports = router
